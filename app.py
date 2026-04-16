483382e28ad37f7cc853f025476a82962513517b
Galc987 <locricchiogabriele231098@gmail.com>
2026-04-16 19:13:24 +0200

    app.py

--- app.py
+++ app.py
@@ -47,4 +47,5 @@ def add_stock():
     return redirect(url_for("home"))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
