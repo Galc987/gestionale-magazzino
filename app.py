b9cd70b759a268709149af4525236cab35b7fff5
Galc987 <locricchiogabriele231098@gmail.com>
2026-04-16 18:59:14 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+products = [
+    {"name": "Catarratto 2L Roberto", "stock": 0},
+    {"name": "Rosato 2L Roberto", "stock": 0},
+    {"name": "Catarratto 2L Francesco", "stock": 0},
+]
+
+orders = []
+production = []
+
+@app.route("/")
+def home():
+    return render_template("index.html", products=products, orders=orders, production=production)
+
+@app.route("/add_order", methods=["POST"])
+def add_order():
+    client = request.form["client"]
+    product = request.form["product"]
+    qty = int(request.form["qty"])
+
+    orders.append({
+        "client": client,
+        "product": product,
+        "qty": qty,
+        "status": "da produrre"
+    })
+
+    production.append({
+        "product": product,
+        "qty": qty
+    })
+
+    return redirect(url_for("home"))
+
+@app.route("/add_stock", methods=["POST"])
+def add_stock():
+    product_name = request.form["product"]
+    qty = int(request.form["qty"])
+
+    for p in products:
+        if p["name"] == product_name:
+            p["stock"] += qty
+
+    return redirect(url_for("home"))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
