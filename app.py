a3ce706ee68884ff3bb08667353e83b7822745a6
Galc987 <locricchiogabriele231098@gmail.com>
2026-04-17 11:39:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ stock = {
 }
 
 orders = []
-deliveries = []
 
 @app.route("/")
 def index():
@@ -23,8 +22,7 @@ def index():
         "index.html",
         clients=clients,
         stock=stock,
-        orders=orders,
-        deliveries=deliveries
+        orders=orders
     )
 
 @app.route("/add_order", methods=["POST"])
@@ -49,21 +47,29 @@ def add_order():
     if items:
         orders.append({
             "client": client,
-            "items": items,
-            "status": "IN_PRODUZIONE"
+            "items": items
         })
 
     return redirect("/")
+
 
 @app.route("/toggle/<int:o>/<int:i>")
 def toggle(o, i):
     orders[o]["items"][i]["done"] = not orders[o]["items"][i]["done"]
     return redirect("/")
 
-@app.route("/complete/<int:index>")
-def complete(index):
-    orders[index]["status"] = "PRONTO"
+
+@app.route("/complete/<int:o>")
+def complete(o):
+
+    for item in orders[o]["items"]:
+        if item["done"]:
+            stock[item["product"]] += item["qty"]
+
+    orders.pop(o)
+
     return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
